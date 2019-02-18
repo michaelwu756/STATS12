21245fc7310dde34f8a2367770a0a36b6432b6d5
Michael Wu <cheeserules43@gmail.com>
2019-02-17 23:44:54 -0800

    finished exercise 2

--- lab3/lab3.R
+++ lab3/lab3.R
@@ -8,3 +8,14 @@ plot(linear_model$residuals ~ soil$zinc, xlab="Zinc Concentration (ppm)",
      ylab="Lead Residuals (ppm)", main = "Lead vs. Zinc in Soil Residuals plot")
 abline(a=0, b=0, col="red", lwd=2)
 
+ice <- read.csv("sea_ice.csv", header = T)
+ice$Date <- as.Date(ice$Date, "%m/%d/%Y")
+linear_model2 <- lm(ice$Extent ~ ice$Date)
+summary(linear_model2)
+plot(ice$Extent ~ ice$Date, xlab="Date (Year)",
+     ylab="Ice Extent (millions of km^2)", main="Ice Extent Over Time", type="l")
+abline(linear_model2, col="red", lwd=2)
+plot(linear_model2$residuals ~ ice$Date, xlab="Date (Year)",
+     ylab="Ice Extent Residuals (millions of km^2)",
+     main = "Ice Extent Over Time Residuals plot")
+abline(a=0, b=0, col="red", lwd=2)
